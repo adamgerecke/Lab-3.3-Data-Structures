45e40ba74c960eaf16bd878bad3ed40a9a7d7179
Adam Gerecke <adamgerecke@quickenloans.com>
2020-07-15 14:40:08 -0400

    Initial Push Prior to any coding. Aiming to practice the commit early commit often method of doing this, rather than last min as I have been doing.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_3._3_Data_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
